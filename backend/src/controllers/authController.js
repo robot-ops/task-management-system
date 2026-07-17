b5f2c71efa99150ed0d873b630ca0f57236eef6d
robot-ops <endugbandhel@gmail.com>
2026-07-18 03:07:11 +0700

    feat(backend): implement user login

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const UserModels = require("../models/authModels");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Create User
 const createUser = async (req, res) => {
@@ -7,7 +8,7 @@ const createUser = async (req, res) => {
 
     if (!body.name || !body.email || !body.password) {
         return res.status(400).json({
-            "message": "field must be filled"
+            "message": "Field must be filled"
         });
     } else {
         try {
@@ -39,7 +40,56 @@ const createUser = async (req, res) => {
     }
 };
 
-const login = async (req, res) => { };
+const login = async (req, res) => {
+    const { body } = req;
+
+    if (!body.email || !body.password) {
+        return res.status(400).json({
+            "message": "Field must be filled"
+        });
+    } else {
+        try {
+            const [user] = await UserModels.getUserByEmail(body.email);
+            if (user.length === 0) {
+                return res.status(404).json({
+                    "message": "User not found"
+                });
+            } else {
+                const isPasswordValid = await bcrypt.compare(body.password, user[0].password);
+                if (!isPasswordValid) {
+                    return res.status(401).json({
+                        "message": "Invalid password"
+                    });
+                } else {
+                    const payload = { 
+                        id: user[0].id, 
+                        email: user[0].email 
+                    };
+
+                    const token = jwt.sign(
+                        payload, 
+                        process.env.JWT_SECRET, 
+                        { expiresIn: "1h" }
+                    );
+                    
+                    res.status(200).json({
+                        "message": "Login successful",
+                        "token": token,
+                        "data": {
+                            "name": user[0].name,
+                            "email": user[0].email
+                        }
+                    });
+                }
+            }
+        } catch (error) {
+            console.error("Error logging in user:", error);
+            res.status(500).json({
+                "message": "Internal server error"
+            });
+        }
+    }
+};
 
 module.exports = {
     createUser,
